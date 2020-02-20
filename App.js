2f049b04b02953628438776547b5d99c010eef40
Georg Preuss <preussg@gmail.com>
2020-02-20 09:22:52 +0100

    redux-like refactor

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import IndexScreen from './src/screens/IndexScreen'
-import { BlogProvider } from './src/context/ BlogContext'
+import { Provider } from './src/context/ BlogContext'
 
 const navigator = createStackNavigator({
   Index: IndexScreen
@@ -17,7 +17,7 @@ const App = createAppContainer(navigator)
 
 // eslint-disable-next-line react/display-name
 export default () => {
-  return <BlogProvider>
+  return <Provider>
     <App />
-  </BlogProvider>
+  </Provider>
 }
