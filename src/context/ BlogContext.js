849ef47c96e8187e8ff4e7cf1eeafb04a19f5c95
Georg Preuss <preussg@gmail.com>
2020-02-20 10:50:49 +0100

    added deleteBlogPost

--- src/context/ BlogContext.js
+++ src/context/ BlogContext.js
@@ -3,7 +3,9 @@ import createDataContext from './createDataContext'
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }]
+      return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }]
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload)
     default:
       return state
   }
@@ -15,4 +17,10 @@ const addBlogPost = (dispatch) => {
   }
 }
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, [])
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id })
+  }
+}
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [])
