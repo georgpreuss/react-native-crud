9517e81affa233697cf087087fccedad8e8b7d01
Georg Preuss <preussg@gmail.com>
2020-02-20 11:04:28 +0100

    added showScreen

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import IndexScreen from './src/screens/IndexScreen'
 import { Provider } from './src/context/ BlogContext'
+import ShowScreen from './src/screens/ShowScreen'
 
 const navigator = createStackNavigator({
-  Index: IndexScreen
+  Index: IndexScreen,
+  Show: ShowScreen
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/ BlogContext'
 import { Feather } from '@expo/vector-icons'
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
   return <View>
@@ -13,12 +13,14 @@ const IndexScreen = () => {
       data={state}
       keyExtractor={(blogPost) => blogPost.title}
       renderItem={({ item }) => {
-        return <View style={styles.row}>
-          <Text style={styles.title}>{item.title} - {item.id}</Text>
-          <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-            <Feather style={styles.icon} name='trash' />
-          </TouchableOpacity>
-        </View>
+        return <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title} - {item.id}</Text>
+            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+              <Feather style={styles.icon} name='trash' />
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
       }}
     />
   </View>
