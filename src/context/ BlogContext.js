6a2759bb6f9d9c303265bd5ce0913e69fb106894
Georg Preuss <preussg@gmail.com>
2020-03-31 12:45:51 +0100

    added edit

--- src/context/ BlogContext.js
+++ src/context/ BlogContext.js
@@ -46,7 +46,9 @@ const deleteBlogPost = dispatch => {
 }
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content })
+    
     dispatch({ type: 'edit_blogpost', payload: { id, title, content } })
     if (callback) {
       callback()
